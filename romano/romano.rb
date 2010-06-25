86bc6030c8ff828fd816de9dfa694c74019b7d3b
rle <rle@rle-desktop.(none)>
2010-06-25 19:26:47 -0300

    refatoracao: usando case -- eduardo

--- romano/romano.rb
+++ romano/romano.rb
@@ -4,13 +4,13 @@ class Romano
   end
 
   def to_roman
-    if @numero == 1
-      "I"
-    elsif @numero == 2
-      "II"
-    elsif @numero == 3
-      "III"
-    end
-    
+    case @numero
+      when 1
+	"I"
+      when 2
+        "II"
+      when 3
+	"III"
+    end    
   end
 end
