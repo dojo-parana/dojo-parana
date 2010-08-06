e6696e42c75a2cae05b4a75b1ba29ec6560dec53
Ramiro Batista da Luz <ramiro@baby-dragon.(none)>
2010-08-06 20:00:03 -0300

    fizzbuzz com 0, 3 e 5 testado

--- fizzbuzz/fizzbuzz.rb
+++ fizzbuzz/fizzbuzz.rb
@@ -1,3 +1,14 @@
 def fizzbuzz(s,e)
-  ["fizzbuzz", " ", "", "fizz"]
+  l = []
+  for i in s..e
+    v = ""
+    if i%3 == 0
+      v += "fizz"
+    end
+    if i%5 == 0
+      v += "buzz"
+    end
+    l << v
+  end
+  return l
 end
--- fizzbuzz/fizzbuzz_test.rb
+++ fizzbuzz/fizzbuzz_test.rb
@@ -9,4 +9,8 @@ class FizzbuzzTest < Test::Unit::TestCase
   def test_3_eh_fizz
     assert_equal(fizzbuzz(0,100)[3], "fizz")
   end
+
+  def test_5_eh_buzz
+    assert_equal(fizzbuzz(0,100)[5], "buzz")
+  end
 end
