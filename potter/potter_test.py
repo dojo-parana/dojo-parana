f2a6d71656a479140aea6ef98db5ed0edbf2f214
coding-dojo <dojo@c3sl.ufpr.br>
2010-11-24 21:33:47 -0200

    refatoracao loop subtracao (usando len)

--- potter/potter_test.py
+++ potter/potter_test.py
@@ -24,7 +24,7 @@ def preco(lista):
 
     min_qtd = min(qtd for livro, qtd in lista)
     res += len(lista) * min_qtd * PRECO * 0.90
-    for i in xrange(3):
+    for i in xrange(len(lista)):
       lista[i][1] -= min_qtd
     res += sum(qtd for l,qtd in lista) * PRECO
 
