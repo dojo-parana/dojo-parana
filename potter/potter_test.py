2e1b410326b8b158497679b3e1dc437b07f7d1fa
coding-dojo <dojo@c3sl.ufpr.br>
2010-11-24 20:08:40 -0200

    teste unico elemento

--- potter/potter_test.py
+++ potter/potter_test.py
@@ -11,6 +11,8 @@ class PotterTest(unittest.TestCase):
     self.assertNotEqual(Potter(), None)
   def test_unico_livro(self):
     self.assertEquals(Potter().preco([[1,1]]),8)
+  def test_dois_livros(self):
+    self.assertEquals(Potter().preco([[1,2]]),16)
 
 if __name__ == '__main__':
     unittest.main()
