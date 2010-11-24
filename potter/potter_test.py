f52d7b53848b1a4f6e6d59b67e3b8874fb9d49af
coding-dojo <dojo@c3sl.ufpr.br>
2010-11-24 20:16:39 -0200

    constante preco

--- potter/potter_test.py
+++ potter/potter_test.py
@@ -1,18 +1,19 @@
 import unittest
 
+PRECO = 8
 def preco(lista):
   if len(lista) == 1:
-    return lista[0][1]*8
+    return lista[0][1]*PRECO
   else:
     return 15.20
 
 class PotterTest(unittest.TestCase):
   def test_unico_livro(self):
-    self.assertEquals(preco([[1,1]]),8)
+    self.assertEquals(preco([[1,1]]),PRECO)
   def test_dois_livros(self):
-    self.assertEquals(preco([[1,2]]),16)
+    self.assertEquals(preco([[1,2]]),PRECO*2)
   def test_dois_livros_diferentes(self):
-    self.assertEquals(preco([[1,1],[2,1]]), 8*2*0.95)
+    self.assertEquals(preco([[1,1],[2,1]]), PRECO*2*0.95)
 
 if __name__ == '__main__':
     unittest.main()
