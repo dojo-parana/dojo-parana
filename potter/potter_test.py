d4be5a378e6dae232599ae1110646286f1d1dd91
coding-dojo <dojo@c3sl.ufpr.br>
2010-11-24 20:32:53 -0200

    adicionado o caso de zero livros

--- potter/potter_test.py
+++ potter/potter_test.py
@@ -6,18 +6,28 @@ def preco(lista):
     return 0
   elif len(lista) == 1:
     return lista[0][1]*PRECO
-  else:
-    return 15.20
+  elif len(lista) == 2:
+    return PRECO*0.95*2 + (lista[0][1] - 1) * PRECO 
 
 class PotterTest(unittest.TestCase):
+
   def test_zero_livros(self):
     self.assertEquals(preco([]),PRECO*0)
+
   def test_unico_livro(self):
     self.assertEquals(preco([[1,1]]),PRECO)
+
   def test_dois_livros(self):
     self.assertEquals(preco([[1,2]]),PRECO*2)
+
   def test_dois_livros_diferentes(self):
     self.assertEquals(preco([[1,1],[2,1]]), PRECO*2*0.95)
+
+  def test_dois_livros_diferentes_quantidades_diferentes(self):
+    self.assertEquals(preco([[1,2],[2,1]]), PRECO*2*0.95+PRECO)
+
+  def test_tres_livros_iguais(self):
+    self.assertEquals(preco([[1,3]]), PRECO*3)
 
 if __name__ == '__main__':
     unittest.main()
