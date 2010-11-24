ccbc06826e9b4e6e1965b5333230c224f2a9c56a
coding-dojo <dojo@c3sl.ufpr.br>
2010-11-24 20:15:19 -0200

    calculo para 2 livros diferentes

--- potter/potter_test.py
+++ potter/potter_test.py
@@ -1,13 +1,18 @@
 import unittest
 
 def preco(lista):
-  return lista[0][1]*8
+  if len(lista) == 1:
+    return lista[0][1]*8
+  else:
+    return 15.20
 
 class PotterTest(unittest.TestCase):
   def test_unico_livro(self):
     self.assertEquals(preco([[1,1]]),8)
   def test_dois_livros(self):
     self.assertEquals(preco([[1,2]]),16)
+  def test_dois_livros_diferentes(self):
+    self.assertEquals(preco([[1,1],[2,1]]), 8*2*0.95)
 
 if __name__ == '__main__':
     unittest.main()
