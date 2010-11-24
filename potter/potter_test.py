e7e5a962c6d15e3d6fae81f579a3e497f9f0d227
coding-dojo <dojo@c3sl.ufpr.br>
2010-11-24 20:12:30 -0200

    refatoracao: faz funcao simples, nao classe

--- potter/potter_test.py
+++ potter/potter_test.py
@@ -1,18 +1,13 @@
 import unittest
 
-class Potter:
-  def __init__(self):
-    pass
-  def preco(self,lista):
-    return lista[0][1]*8
+def preco(lista):
+  return lista[0][1]*8
 
 class PotterTest(unittest.TestCase):
-  def test_create_potter(self):
-    self.assertNotEqual(Potter(), None)
   def test_unico_livro(self):
-    self.assertEquals(Potter().preco([[1,1]]),8)
+    self.assertEquals(preco([[1,1]]),8)
   def test_dois_livros(self):
-    self.assertEquals(Potter().preco([[1,2]]),16)
+    self.assertEquals(preco([[1,2]]),16)
 
 if __name__ == '__main__':
     unittest.main()
