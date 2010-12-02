fb51a4c5ceed1676755b6a19d4bf15f3bc0edb05
Coding Dojo Parana <dojo-parana@blue-dragon.firehouse.net>
2010-12-02 20:06:42 -0200

    mais 2 outros casos

--- anagrama/anagrama_test.py
+++ anagrama/anagrama_test.py
@@ -7,7 +7,7 @@ def test_anagram(palavra1, palavra2, chars):
     for c in palavra1+palavra2:
         if not c in chars:
             return False
-    return True
+    return len(palavra1+palavra2) == len(chars)
 
 class AnagramaTest(unittest.TestCase):
     def test_anagram_test(self):
@@ -15,6 +15,10 @@ class AnagramaTest(unittest.TestCase):
         self.assertFalse(test_anagram('a', 'x', 'ab'))
         self.assertTrue(test_anagram('ab', 'cd', 'abcd'))
         self.assertTrue(test_anagram('ac', 'bd', 'abcd'))
+
+    def test_anagram_letras_repetidas(self):
+        self.assertTrue(test_anagram('aab', 'bbc', 'aabbbc'))
+        self.assertFalse(test_anagram('aab', 'bbc', 'abc'))
 
 if __name__ == '__main__':
    unittest.main()
