b76501ad9780c3ff9b25825e1cfee82562e3c664
Coding Dojo Parana <dojo-parana@blue-dragon.firehouse.net>
2010-12-02 19:59:52 -0200

    primeira versao do anagrama. primeiro teste simples

---
+++ anagrama/anagrama_test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding=utf-8 -*- 
+
+import unittest
+
+def test_anagram(palavra1, palavra2, chars):
+    return True
+
+class AnagramaTest(unittest.TestCase):
+    def test_anagram_test(self):
+        self.assertTrue(test_anagram('a', 'b', 'ab'))
+
+if __name__ == '__main__':
+   unittest.main()
