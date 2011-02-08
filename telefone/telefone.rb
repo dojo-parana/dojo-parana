fe3d26a932791d04f239d7b6e924ed754f956ce8
dojo <dojo@wetzel.nullable.org>
2011-02-08 20:57:34 -0200

    Recriado o teste de abertura do arquivo.

--- telefone/telefone.rb
+++ telefone/telefone.rb
@@ -1,8 +1,9 @@
 require "test/unit"
 
 class Telefone
+  attr_accessor :file
   def initialize
-    File.open('/home/dojo/sandbox/dojo-parana/telefone/arquivo.txt', 'r')
+    @file = File.open('/home/dojo/sandbox/dojo-parana/telefone/arquivo.txt', 'r')
   end
 end
 
@@ -12,6 +13,6 @@ class TelefoneTest < Test::Unit::TestCase
   end
   
   def teste_open_file
-    assert_not_nil(Telefone.new)
+    assert_equal(File === Telefone.new.file, true)
   end
 end
