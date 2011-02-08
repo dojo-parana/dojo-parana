a25db9e9e0ab15feb2250daaf707937667c5389b
dojo <dojo@wetzel.nullable.org>
2011-02-08 21:10:34 -0200

    A vira 2

--- telefone/telefone.rb
+++ telefone/telefone.rb
@@ -1,10 +1,15 @@
 require "test/unit"
 
 class Telefone
-  attr_accessor :file
+  attr_accessor :file, :content
   def initialize
     @file = File.open('/home/dojo/sandbox/dojo-parana/telefone/arquivo.txt', 'r')
+    @content = @file.read
   end
+  
+    def processar_linha s
+        "2"
+    end 
 end
 
 class TelefoneTest < Test::Unit::TestCase
@@ -14,5 +19,17 @@ class TelefoneTest < Test::Unit::TestCase
   
   def teste_open_file
     assert_equal(File === Telefone.new.file, true)
+  end
+  
+  def test_read_file
+    assert_not_nil(Telefone.new.content)
+  end
+  
+  def test_content_not_empty
+    assert_not_equal(Telefone.new.content, "")
+  end
+  
+  def test_substituicao
+    assert_equal(Telefone.new.processar_linha("A"), "2")
   end
 end
