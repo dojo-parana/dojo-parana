ac371c317fb85588df40a46413448712cea47b9e
Coding Dojo Parana <dojo-parana@googlegroups.com>
2011-02-22 20:58:57 -0300

    Teste converte inteiro para palavra, Ramiro, Rafael.

--- contaletras/contaletras.py
+++ contaletras/contaletras.py
@@ -1,14 +1,19 @@
 # -*- coding:utf-8 -*-
 
+'''
+Se os números de 1 a 5 fossem escritos em palavras: um, dois, três, quatro, cinco, então teríamos utilizado 2 + 4 + 4 + 6 + 5 = 21 letras no total.
+Se todos os números de 1 até 1000 fossem escritos em palavras, quantas letras nós teríamos utilizado?
+'''
+
 import unittest
 
 class ContaLetras:
-  def __init__(self):
-    pass
+    def int_2_palavra(self, numero):
+        return 'um'
 
 class ContaLetrasTest(unittest.TestCase):
-  def test_create_contaletras(self):
-    self.assertNotEqual(ContaLetras(), None)
+    def test_int_2_palavra(self):
+        self.assertEqual(ContaLetras().int_2_palavra(1), 'um')
 
 if __name__ == '__main__':
     unittest.main()    
