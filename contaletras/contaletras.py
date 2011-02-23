3263ab5e8ba5d280ad85ddf72f3f6f96c309d1fb
Coding Dojo Parana <dojo-parana@googlegroups.com>
2011-02-22 21:21:02 -0300

    Teste converte inteiros até 9 para palavra usando dicionario, Arthur, Paulo.

--- contaletras/contaletras.py
+++ contaletras/contaletras.py
@@ -15,6 +15,11 @@ class ContaLetras:
             2: 'dois',
             3: 'tres',
             4: 'quatro',
+            5: 'cinco',
+            6: 'seis',
+            7: 'sete',
+            8: 'oito',
+            9: 'nove',        
         }
         
         return dicionario[numero]
@@ -32,7 +37,21 @@ class ContaLetrasTest(unittest.TestCase):
     def test_int_4_para_palavra(self):
         self.assertEqual(ContaLetras().int_2_palavra(4), 'quatro')
         
-            
+    def test_int_5_para_palavra(self):
+        self.assertEqual(ContaLetras().int_2_palavra(5), 'cinco')
         
+    def test_int_6_para_palavra(self):
+        self.assertEqual(ContaLetras().int_2_palavra(6), 'seis')
+        
+    def test_int_7_para_palavra(self):
+        self.assertEqual(ContaLetras().int_2_palavra(7), 'sete')
+
+    def test_int_8_para_palavra(self):
+        self.assertEqual(ContaLetras().int_2_palavra(8), 'oito')
+        
+    def test_int_9_para_palavra(self):
+        self.assertEqual(ContaLetras().int_2_palavra(9), 'nove')
+        
+
 if __name__ == '__main__':
     unittest.main()    
