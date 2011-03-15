af54e1a65f4f857c3258059966975ac9eee0a8d1
Coding Dojo Parana <dojo-parana@googlegroups.com>
2011-03-15 20:34:59 -0300

    Add the initial script of the dojo.

---
+++ caixa/caixa.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# automatically created by Dojo Polyglot Environ on 2011-03-15 20:30:39
+# https://github.com/afurlan/dojo-polyglot-environ
+
+import unittest2 as unittest
+
+class Caixa:
+    def __init__(self):
+        pass
+
+class CaixaTest(unittest.TestCase):
+    def test_init(self):
+        self.assertNotEqual(Caixa(), None)
+
+if __name__ == '__main__':
+    unittest.main()
