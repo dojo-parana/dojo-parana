52fd8b8d28bd11a50891ae74ba83437e3d18877f
Coding Dojo Parana <dojo-parana@googlegroups.com>
2011-03-15 21:17:05 -0300

    Criado teste para valor 40, Thiago, Juliano

--- caixa/caixa.py
+++ caixa/caixa.py
@@ -9,11 +9,18 @@ import unittest2 as unittest
 class Caixa:
     def __init__(self):
         pass
+    
     def sacar_notas(self, valor):
-        if valor < 100:
-            return [50]
-
         notas = []
+        
+        if valor < 50:
+            for x in xrange(valor/20):
+                notas.append(20)
+        
+        if valor < 100:
+            for x in xrange(valor/50):
+                notas.append(50)
+
         for x in xrange(valor/100):
             notas.append(100)
         return notas
@@ -34,6 +41,11 @@ class CaixaTest(unittest.TestCase):
     def test_nota50(self):
         self.assertEqual(Caixa().sacar_notas(50), [50])
 
+    def test_nota20(self):
+        self.assertEqual(Caixa().sacar_notas(20), [20])
+
+    def test_nota40(self):
+        self.assertEqual(Caixa().sacar_notas(40), [20,20])
 
 if __name__ == '__main__':
     unittest.main()
