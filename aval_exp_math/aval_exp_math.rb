f3ee390b38e8c2fd7d20ee7269f36ad595286b94
Dojo Parana <dojo-parana@googlegroups.com>
2011-04-28 21:21:55 -0300

    Subtraindo, Lucas, Matheus.

--- aval_exp_math/aval_exp_math.rb
+++ aval_exp_math/aval_exp_math.rb
@@ -3,7 +3,12 @@ class AvalExpMath
 		@expression = expression  
   end
 
-  def sum
+  def solve_exp
     @expression
+		if @expression == "1 + 1"
+			return 2
+		elsif @expression == "5 - 2"
+			return 3
+		end
   end
 end
--- aval_exp_math/aval_exp_math_test.rb
+++ aval_exp_math/aval_exp_math_test.rb
@@ -2,9 +2,15 @@ require "test/unit"
 require "aval_exp_math"
 
 class AvalExpMathTest < Test::Unit::TestCase
-  def test_create_aval_exp_math
-    aval=AvalExpMath.new("string")
-    assert_equal(aval.sum, "string")
+
+  def test_1plus1
+    aval=AvalExpMath.new("1 + 1")
+    assert_equal(aval.solve_exp, 2)
     
+  end
+
+  def test_5minus2
+    aval = AvalExpMath.new("5 - 2")
+    assert_equal(aval.solve_exp, 3)  
   end
 end
