e3b472007e827d9e754b585e8cddc3c7674793b1
Ramiro Batista da Luz <ramiroluz@gmail.com>
2011-08-24 19:14:29 -0300

    Inicio do dojo

---
+++ matriz_espiral/matriz_espiral.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# automatically created by Dojo Polyglot Environ on 2011-08-24 19:13:39
+# https://github.com/afurlan/dojo-polyglot-environ
+
+import unittest2 as unittest
+
+class matriz_espiral:
+    def __init__(self):
+        pass
+
+class matriz_espiralTest(unittest.TestCase):
+    def test_init(self):
+        self.assertNotEqual(matriz_espiral(), None)
+
+if __name__ == '__main__':
+    unittest.main()
