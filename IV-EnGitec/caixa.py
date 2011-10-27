2283ac4f64ec83e9da6c764167bae4f6e8b4be0c
Ramiro Batista da Luz <ramiroluz@gmail.com>
2011-10-27 11:40:32 -0200

    Código final. chajun21@gmail.com, contato@gilsondev.com, star.ill.nino@gmail.com, adrianorg@gmail.com

--- IV-EnGitec/caixa.py
+++ IV-EnGitec/caixa.py
@@ -9,13 +9,20 @@ import unittest2 as unittest
 
 class Caixa:
     def __init__(self):
-        self.notas_padrao = [10,20,50,100]
+        self.notas_padrao = [100,50,20,10]
 
     def sacar(self,valor):
-        if valor in self.notas_padrao:
-			return {valor:1}
-        else:
+        dic = {}
+        aux = valor
+        for nota in self.notas_padrao:
+            cont = aux // nota 
+            if cont:
+                dic[nota] = cont
+            aux = aux % nota
+        if aux:
 		    return False
+        return dic
+        
 		
  
 class CaixaTest(unittest.TestCase):
@@ -37,6 +44,21 @@ class CaixaTest(unittest.TestCase):
 
     def test_saque_nota100(self):
         self.assertEqual(self.caixa.sacar(100),{100:1})
+
+    def test_saque_valor30(self):
+        self.assertEqual(self.caixa.sacar(30),{20:1,10:1})
+
+    def test_saque_valor80(self):
+        self.assertEqual(self.caixa.sacar(80),{50:1,20:1,10:1})
+
+    def test_saque_valor55(self):
+        self.assertFalse(self.caixa.sacar(55))
+
+    def test_saque_valor680(self):
+        self.assertEqual(self.caixa.sacar(680),{100:6,50:1,20:1,10:1})
+
+    def test_saque_valor682(self):
+        self.assertFalse(self.caixa.sacar(682))
 
 if __name__ == '__main__':
     unittest.main()
