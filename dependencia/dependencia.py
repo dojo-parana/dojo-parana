4820b7d3cd3093cd469bd0e7208e266605b64fcc
Ramiro Batista da Luz <ramiroluz@gmail.com>
2012-01-19 20:41:32 -0200

    Código exemplo de testes, primeiro commit antes de iniciar o dojo.

---
+++ dependencia/dependencia.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# automatically created by Dojo Polyglot Environ on 2012-01-19 20:34:12
+# https://github.com/afurlan/dojo-polyglot-environ
+
+import unittest2 as unittest
+
+class dependencia:
+    def __init__(self):
+        pass
+
+class dependenciaTest(unittest.TestCase):
+    def test_init(self):
+        self.assertNotEqual(dependencia(), None)
+
+if __name__ == '__main__':
+    unittest.main()
