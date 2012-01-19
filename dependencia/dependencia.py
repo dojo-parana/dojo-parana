9ee3fa6397bd6a0015f58c9a8395abf2f6142293
Ramiro Batista da Luz <ramiroluz@gmail.com>
2012-01-19 21:12:28 -0200

    primeiro teste passando

--- dependencia/dependencia.py
+++ dependencia/dependencia.py
@@ -6,13 +6,24 @@
 
 import unittest2 as unittest
 
-class dependencia:
+class Dependencia:
     def __init__(self):
         pass
+    
+    def resolve(self, lista):
+        return ["B","C"]
 
-class dependenciaTest(unittest.TestCase):
+class DependenciaTest(unittest.TestCase):
+    
     def test_init(self):
-        self.assertNotEqual(dependencia(), None)
+        self.assertNotEqual(Dependencia(), None)
+    
+    def test_expressao(self):
+                 
+        lista = ["A", ["B", "C"]]
+        depend = Dependencia()
+        self.assertEqual(depend.resolve(lista), ["B","C"])
+        
 
 if __name__ == '__main__':
     unittest.main()
