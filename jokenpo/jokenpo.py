5a21caa1d83e34f7d62ba59a327eb634b6035428
Ramiro Batista da Luz <ramiroluz@gmail.com>
2012-07-23 14:41:54 -0300

    Esqueleto do código, gerado pelo dojo-polyglot-environ

---
+++ jokenpo/jokenpo.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# automatically created by Dojo Polyglot Environ on 2012-07-23 14:23:17
+# https://github.com/afurlan/dojo-polyglot-environ
+
+import unittest2 as unittest
+
+class jokenpo:
+    def __init__(self):
+        pass
+
+class jokenpoTest(unittest.TestCase):
+    def test_init(self):
+        self.assertNotEqual(jokenpo(), None)
+
+if __name__ == '__main__':
+    unittest.main()
