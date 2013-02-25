f71ea9466783519040833dc5127216866b68eb4e
Ramiro Batista da Luz <ramiroluz@gmail.com>
2013-02-25 20:37:39 -0300

    Cleverson e Paulo

--- livraria_hp/hp.py
+++ livraria_hp/hp.py
@@ -22,29 +22,27 @@ def calcula(lista):
 
     return total
 
-class hpTest(unittest.TestCase):
-    def test_um(self):
+class calculoDescontoProgressivoTest(unittest.TestCase):
+    def test_uma_copia(self):
         self.assertEqual(PrecoLivro, calcula([1]))
 
-    def test_dois(self):
+    def test_duas_copias(self):
         self.assertEqual((PrecoLivro*2)*0.95, calcula([2]))
 
-    def test_um_um(self):
+    def test_duas_copias_distintas(self):
         self.assertEqual((PrecoLivro*2), calcula([1,1]))
 
-    def test_dois_um(self):
+    def test_duas_copias_mais_uma_distinta(self):
         self.assertEqual(42 + 79.8, calcula([2,1]))
 
-    def test_dois_dois(self):
+    def test_duas_copias_mais_duas(self):
         self.assertEqual(79.8+ 79.8, calcula([2,2]))
 
-    def test_um_dois_um(self):
+    def test_uma_copia_mais_duas_mais_uma(self):
         self.assertEqual(PrecoLivro + 79.8 + PrecoLivro, calcula([1,2,1]))
     
-    def test_tres(self):
+    def test_tres_copias(self):
         self.assertEqual(113.4, calcula([3]))
-
-    
 			
 if __name__ == '__main__':
     unittest.main()
