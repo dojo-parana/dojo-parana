9355738f6111068c22c610b8d68fb61d1ab46209
Ramiro Batista da Luz <ramiroluz@gmail.com>
2013-02-25 20:23:18 -0300

    Everaldo e Cassio

--- livraria_hp/hp.py
+++ livraria_hp/hp.py
@@ -11,29 +11,28 @@ PrecoLivro = 42
 def calcula(lista):
     total = 0
 
-    for qtde,volume in lista:
+    for qtde in lista:
         if qtde == 1:
             total += PrecoLivro
         elif qtde == 2:
             total += PrecoLivro * 2 * 0.95
-        
-
-
-    
     return total
 
 class hpTest(unittest.TestCase):
     def test_uma_copia_livro_um(self):
-        self.assertEqual(PrecoLivro, calcula([(1, 1)]))
+        self.assertEqual(PrecoLivro, calcula([1]))
 
     def test_duas_copias_livro_um(self):
-        self.assertEqual((PrecoLivro*2)*0.95, calcula([(2,1)]))
+        self.assertEqual((PrecoLivro*2)*0.95, calcula([2]))
 
-    def test_uma_copias_livro_um_uma_copia_livro_dois(self):
-        self.assertEqual((PrecoLivro*2), calcula([(1,1),(1,2)]))
+    def test_uma_copia_livro_um_uma_copia_livro_dois(self):
+        self.assertEqual((PrecoLivro*2), calcula([1,1]))
 
     def test_duas_copias_livro_um_uma_copia_livro_dois(self):
-        self.assertEqual(42 + 79.8, calcula([(2,1),(1,2)]))
+        self.assertEqual(42 + 79.8, calcula([2,1]))
+
+    def test_duas_copias_livro_duas_copias_livro_dois(self):
+        self.assertEqual(79.8+ 79.8, calcula([2,2]))
 
 			
 if __name__ == '__main__':
