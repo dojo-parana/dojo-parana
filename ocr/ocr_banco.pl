f7634df817edbc6290abbbb602a6fdd1209b1b2e
Ramiro Batista da Luz <ramiroluz@gmail.com>
2013-04-15 20:30:41 -0300

    comparacao utilizando 1,2,3 - Marcelo/Keuller

--- ocr/ocr_banco.pl
+++ ocr/ocr_banco.pl
@@ -16,19 +16,26 @@ my $dois =
 " _|\n" .
 "|_ \n   ";
 
+my $tres =
+" _ \n" .
+" _|\n" .
+" _|\n   ";
+
 sub parse{
     my $arg = shift(@_);
 
     if ($arg eq $dois) {
        return 2;
     }
-
+    if ($arg eq $tres) {
+       return 3;
+    }
         
     return 1;
 }
 
 ok(parse($um) == 1, "um");
 ok(parse($dois) == 2, "dois");
-ok(parse($dois) == 3, "tres");
+ok(parse($tres) == 3, "tres");
 
 done_testing();
