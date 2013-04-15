c90ebaafb144fc71a79c432f95918741f491d531
Ramiro Batista da Luz <ramiroluz@gmail.com>
2013-04-15 20:28:22 -0300

    comparacao utilizando 1 e 2 - Marcelo/Keuller

---
+++ ocr/ocr_banco.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/env perl
+use warnings;
+use strict;
+
+use Test::More;
+
+#is(1, 2, "teste");
+
+my $um =
+"   \n". 
+"  |\n".
+"  |\n   ";
+
+my $dois =
+" _ \n" .
+" _|\n" .
+"|_ \n   ";
+
+sub parse{
+    my $arg = shift(@_);
+
+    if ($arg eq $dois) {
+       return 2;
+    }
+
+        
+    return 1;
+}
+
+ok(parse($um) == 1, "um");
+ok(parse($dois) == 2, "dois");
+
+done_testing();
