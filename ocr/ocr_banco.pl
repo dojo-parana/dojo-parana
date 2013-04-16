60217d3fe28963a35fdfb2e971646e8e128cd4fa
Ramiro Batista da Luz <ramiroluz@gmail.com>
2013-04-15 21:50:09 -0300

    versao final

--- ocr/ocr_banco.pl
+++ ocr/ocr_banco.pl
@@ -7,21 +7,32 @@ use Test::More;
 #is(1, 2, "teste");
 
 my $um =
-"   \n". 
-"  |\n".
-"  |\n   ";
+"   ". 
+"  |".
+"  |";
 
 my $dois =
-" _ \n" .
-" _|\n" .
-"|_ \n   ";
+" _ " .
+" _|" .
+"|_ ";
 
 my $tres =
-" _ \n" .
-" _|\n" .
-" _|\n   ";
+" _ " .
+" _|" .
+" _|";
 
-sub parse{
+my $doze =
+"    _ \n" .
+"  | _|\n" .
+"  ||_ \n      ";
+
+
+my $treze = 
+"    _ \n" .
+"  | _|\n" .
+"  | _|\n      ";
+
+sub parse {
     my $arg = shift(@_);
 
     if ($arg eq $dois) {
@@ -29,13 +40,36 @@ sub parse{
     }
     if ($arg eq $tres) {
        return 3;
-    }
-        
+    }   
     return 1;
+}
+
+sub splitArgs {
+    my $arg = shift(@_);
+    my @digitos = ('', '');
+
+    
+    my @linhas  = split("\n",$arg);
+
+    for my $linha(@linhas){
+        my $digito = 0;
+        while ($linha =~ m/(.{3})/g){
+             $digitos[$digito++] .= $1;
+        }
+    }
+
+    return @digitos;
 }
 
 ok(parse($um) == 1, "um");
 ok(parse($dois) == 2, "dois");
 ok(parse($tres) == 3, "tres");
 
+my @retorno_doze = ($um,$dois);
+my @retorno_treze = ($um,$tres);
+
+is_deeply(splitArgs($doze), @retorno_doze, "split 12");
+is_deeply(splitArgs($treze), @retorno_treze, "split 13");
+
 done_testing();
+
