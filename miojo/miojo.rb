5d74cb805a52e7bb16e187e1630d389e654f3473
Ramiro Batista da Luz <ramiroluz@gmail.com>
2013-05-27 19:40:53 -0300

    Initial miojo commit
    Milton Soares
    Rafael Santos

---
+++ miojo/miojo.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+#
+# automatically created by Dojo Polyglot Environ on 2013-05-27 19:35:46
+# https://github.com/afurlan/dojo-polyglot-environ
+
+require "test/unit"
+
+class miojo
+    def initialize
+    end
+end
+
+class Testmiojo < Test::Unit::TestCase
+    def test_initialize
+        assert_not_nil(miojo.new)
+    end
+end
