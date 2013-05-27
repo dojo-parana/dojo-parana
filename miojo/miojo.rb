3599211bcf78daf393c2f1dcaa127c952a973d02
Ramiro Batista da Luz <ramiroluz@gmail.com>
2013-05-27 19:52:46 -0300

    Primeiro método dos testes usando o exemplo dos sites
    Milton Soares
    Rafael Santos

--- miojo/miojo.rb
+++ miojo/miojo.rb
@@ -8,10 +8,20 @@ require "test/unit"
 class Miojo
     def initialize
     end
+
+    def preparo(cozimento, ampulheta1, ampulheta2)
+        10
+    end
 end
 
 class TestMiojo < Test::Unit::TestCase
     def test_initialize
         assert_not_nil(Miojo.new)
+    end
+
+    def test_default
+        miojo = Miojo.new
+        tempo = miojo.preparo(3, 5, 7)
+        assert_equal(10, tempo)
     end
 end
