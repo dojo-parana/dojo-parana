b491507e020c49175a0dc33aaa718551fe1252d1
Ramiro Batista da Luz <ramiroluz@gmail.com>
2013-10-28 20:43:30 -0200

    Segundo teste passando. Heigler e Delvair

--- pizza/test_pizza.py
+++ pizza/test_pizza.py
@@ -9,5 +9,8 @@ class PizzaTest(unittest.TestCase):
     def test_luca_deve_ser_renato(self):
         self.assertEqual(pizza.compatibilidade('Luca'), 'Renato')
 
+    def test_lenon_deve_ser_renata(self):
+        self.assertEqual(pizza.compatibilidade('Lenon'), 'Renata')        
+
 if __name__ == '__main__':
     unittest.main()
