750fe055a69008112dc8e3e4da759145b58bad89
Ramiro Batista da Luz <ramiroluz@gmail.com>
2013-10-28 20:31:50 -0200

    Primeiro teste passando. Davi e Nei

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python
 # -*- coding=utf-8 -*-
 
+equipe = {'Renato' : { 'Marguerita' : 4, 'Quatro queijos' : 5, 'Escarola' : 4, 'Portuguesa' : 5, 'Frango+Catupiry' : 4, 'Napolitana' : 3 },
+    'Marcelo' : { 'Marguerita' : 2, 'Quatro queijos' : 2, 'Escarola' : 1, 'Portuguesa' : 3, 'Frango+Catupiry' : 5, 'Napolitana' : 2 },
+    'Lenon' : { 'Marguerita' : 4, 'Quatro queijos' : 5, 'Escarola' : 2, 'Portuguesa' : 1, 'Frango+Catupiry' : 1, 'Napolitana' : 3 },
+    'Renata' : { 'Marguerita' : 4, 'Quatro queijos' : 5, 'Escarola' : 1, 'Portuguesa' : 1, 'Frango+Catupiry' : 3, 'Napolitana' : 4 },
+    'Washington' : { 'Marguerita' : 1, 'Quatro queijos' : 1, 'Escarola' : 2, 'Portuguesa' : 3, 'Frango+Catupiry' : 4, 'Napolitana' : 3 },
+    'Tino' : { 'Marguerita' : 1, 'Quatro queijos' : 5, 'Escarola' : 1, 'Portuguesa' : 4, 'Frango+Catupiry' : 3, 'Napolitana' : 2 },
+    'Luca' : { 'Marguerita' : 5, 'Quatro queijos' : 4, 'Escarola' : 3, 'Portuguesa' : 4, 'Frango+Catupiry' : 3, 'Napolitana' : 2 }
+}
 
+
+def compatibilidade(nome):
+    return 'Renato'
--- pizza/test_pizza.py
+++ pizza/test_pizza.py
@@ -3,8 +3,11 @@
 
 import unittest
 
-class PizzaTest():
-    pass
+import pizza
+
+class PizzaTest(unittest.TestCase):
+    def test_luca_deve_ser_renato(self):
+        self.assertEqual(pizza.compatibilidade('Luca'), 'Renato')
 
 if __name__ == '__main__':
     unittest.main()
