de9840fa3b5dfa0b24fba09bbb0ccd0f01030754
Leandro Henrique Stein <leandro@daileon.jaspion>
2013-11-21 20:46:49 -0200

    Passou nos testes 5 - Allan e Luciano.

--- le_cheque/test_cheque.py
+++ le_cheque/test_cheque.py
@@ -8,11 +8,13 @@ class TestCheque(unittest.TestCase):
     def test_um(self):
         self.assertEqual(Conversor().bla('um'), '1')
     def test_dois(self):
-		self.assertEqual(Conversor().bla('dois'), '2')
+        self.assertEqual(Conversor().bla('dois'), '2')
     def test_tres(self):
-		self.assertEqual(Conversor().bla('três'), '3')
+        self.assertEqual(Conversor().bla('três'), '3')
     def test_quatro(self):
-		self.assertEqual(Conversor().bla('quatro'), '4')
+        self.assertEqual(Conversor().bla('quatro'), '4')
+    def test_cinco(self):
+        self.assertEqual(Conversor().bla('cinco'), '5')
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
