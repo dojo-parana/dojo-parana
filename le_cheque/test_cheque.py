fda9d4783d6356985367926794e97cb5f3db3020
Leandro Henrique Stein <leandro@daileon.jaspion>
2013-11-21 21:04:57 -0200

    Passou nos teste cem - Musis e Debora

--- le_cheque/test_cheque.py
+++ le_cheque/test_cheque.py
@@ -19,6 +19,8 @@ class TestCheque(unittest.TestCase):
         self.assertEqual(Conversor().bla('vinte'), '20')
     def test_doze(self):
         self.assertEqual(Conversor().bla('doze'), '12')    
+    def test_cem(self):
+        self.assertEqual(Conversor().bla('cem'), '100')
     
 
 if __name__ == '__main__':
