afb8791d646af315f001ed6eef1daf1d758e43ba
Leandro Henrique Stein <leandro@daileon.jaspion>
2013-11-21 21:16:08 -0200

    Refatoracao do tipo de retorno - Leandro e Ramiro

--- le_cheque/test_cheque.py
+++ le_cheque/test_cheque.py
@@ -6,29 +6,29 @@ from conversor import Conversor
 
 class TestCheque(unittest.TestCase):
     def test_um(self):
-        self.assertEqual(Conversor().bla('um'), '1')
+        self.assertEqual(Conversor().bla('um'), 1)
     def test_dois(self):
-        self.assertEqual(Conversor().bla('dois'), '2')
+        self.assertEqual(Conversor().bla('dois'), 2)
     def test_tres(self):
-        self.assertEqual(Conversor().bla('três'), '3')
+        self.assertEqual(Conversor().bla('três'), 3)
     def test_quatro(self):
-        self.assertEqual(Conversor().bla('quatro'), '4')
+        self.assertEqual(Conversor().bla('quatro'), 4)
     def test_cinco(self):
-        self.assertEqual(Conversor().bla('cinco'), '5')
+        self.assertEqual(Conversor().bla('cinco'), 5)
     def test_vinte(self):
-        self.assertEqual(Conversor().bla('vinte'), '20')
+        self.assertEqual(Conversor().bla('vinte'), 20)
     def test_doze(self):
-        self.assertEqual(Conversor().bla('doze'), '12')    
+        self.assertEqual(Conversor().bla('doze'), 12)    
     def test_cem(self):
-        self.assertEqual(Conversor().bla('cem'), '100')
+        self.assertEqual(Conversor().bla('cem'), 100)
     def test_mil(self):
-        self.assertEqual(Conversor().bla('mil'), '1000')
+        self.assertEqual(Conversor().bla('mil'), 1000)
     def test_cento(self):
-        self.assertEqual(Conversor().bla('cento'), '100')
+        self.assertEqual(Conversor().bla('cento'), 100)
     def test_centoequinze(self):
-        self.assertEqual(Conversor().bla('cento e quinze'), '115')
+        self.assertEqual(Conversor().bla('cento e quinze'), 115)
     def test_centoedez(self):
-        self.assertEqual(Conversor().bla('cento e dez'), '110')
+        self.assertEqual(Conversor().bla('cento e dez'), 110)
     
 
 if __name__ == '__main__':
