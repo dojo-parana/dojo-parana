00df459f10231da6e8a7ccca1cd27b5e7ea12c97
Ramiro Batista da Luz <ramiroluz@gmail.com>
2013-12-19 20:48:42 -0200

    Paulo-Luciano

--- hanoi/torre.py
+++ hanoi/torre.py
@@ -1,3 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
+
+class Pino(object):
+    def __init__(self):
+        pass
--- hanoi/torre_test.py
+++ hanoi/torre_test.py
@@ -4,11 +4,14 @@
 
 import unittest
 import torre
+from torre import Pino
 
 
-class calculoDescontoProgressivoTest(unittest.TestCase):
-   def test_exemplo(self):
-        self.assertTrue(True)
-		
+class pinoTeste(unittest.TestCase):
+    def test_exemplo(self):
+        pino = Pino()
+        self.assertTrue(pino)
+
+
 if __name__ == '__main__':
     unittest.main()	
