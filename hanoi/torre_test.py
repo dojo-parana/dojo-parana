df1b78983c7ac5bad013aee1e4ae4db522e2aa97
Ramiro Batista da Luz <ramiroluz@gmail.com>
2013-12-19 20:26:53 -0200

    Esqueleto inicial do código

---
+++ hanoi/torre_test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+
+import unittest
+import torre
+
+
+class calculoDescontoProgressivoTest(unittest.TestCase):
+   def test_exemplo(self):
+        self.assertTrue(True)
+		
+if __name__ == '__main__':
+    unittest.main()	
