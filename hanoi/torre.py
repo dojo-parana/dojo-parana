f4da5a74a18b822862d699e1c30bb323f6f043e6
Ramiro Batista da Luz <ramiroluz@gmail.com>
2013-12-19 21:55:57 -0200

    João-Paulo

--- hanoi/torre.py
+++ hanoi/torre.py
@@ -6,6 +6,9 @@ class Pino(object):
     def __init__(self, numero):
         self.numero = numero
         self.discos = []
+        self.adicionaDisco(Disco(1))
+        self.adicionaDisco(Disco(2))
+        self.adicionaDisco(Disco(3))
 
     def adicionaDisco(self, disco):
         self.discos.append(disco)
@@ -17,6 +20,14 @@ class Disco(object):
 class Tabuleiro(object):
     def __init__(self):
         self.pinos = []
+        self.adicionaPino(Pino(1))
+        self.adicionaPino(Pino(2))
+        self.adicionaPino(Pino(3))
+        self.pino = Pino(1)
+        self.pino.adicionaDisco(Disco(3))
+        self.pino.adicionaDisco(Disco(2))
+        self.pino.adicionaDisco(Disco(1))
+
 
     def adicionaPino(self, pino):
         self.pinos.append(pino)
--- hanoi/torre_test.py
+++ hanoi/torre_test.py
@@ -11,13 +11,6 @@ from torre import Tabuleiro
 class pinoTeste(unittest.TestCase):
     def setUp(self):
         self.tabuleiro = Tabuleiro()
-        self.tabuleiro.adicionaPino(Pino(1))
-        self.tabuleiro.adicionaPino(Pino(2))
-        self.tabuleiro.adicionaPino(Pino(3))
-        self.pino = Pino(1)
-        self.pino.adicionaDisco(Disco(3))
-        self.pino.adicionaDisco(Disco(2))
-        self.pino.adicionaDisco(Disco(1))
 
     def test_pino_existe(self):
         pino = Pino(1)
@@ -28,7 +21,7 @@ class pinoTeste(unittest.TestCase):
         self.assertEqual(1, disco.tamanho)
 
     def test_discos_no_pino(self):
-        self.assertEqual(3, len(self.pino.discos))
+        self.assertEqual(3, len(self.tabuleiro.pinos[0].discos))
 
     def test_tabuleiro_existe(self):
         tabuleiro = Tabuleiro()
