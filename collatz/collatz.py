fd4a19bf69e3487804baf767b27d891f4f2379ba
Ramiro Batista da Luz <ramiroluz@gmail.com>
2014-11-04 13:09:48 -0200

    Pedro, Leon

--- collatz/collatz.py
+++ collatz/collatz.py
@@ -1,15 +1,30 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-class Collatz():
-   def retorna_sequencia(self, valor):
-       res=[]
-       while valor != 1:
-           res.append(valor)
+class Collatz(int):
 
-           if valor % 2 == 0:
-              valor = valor/2
-           else:
-              valor = (valor * 3) + 1
-       res.append(1)
-       return tuple(res)
+    def __init__(self, valor):
+        self = valor
+
+    def retorna_sequencia(self):
+        res=[]
+        valor = self
+
+        while valor != 1:
+            res.append(valor)
+
+            if valor % 2 == 0:
+               valor = valor/2
+            else:
+               valor = (valor * 3) + 1
+        res.append(1)
+        return tuple(res)
+
+    def maior_sequencia(self):
+        maior = tuple()
+        for numero in range(self):
+            sequencia = Collatz(numero+1).retorna_sequencia()
+            if len(sequencia) > len(maior):
+                maior = sequencia
+
+        return maior
--- collatz/test_collatz.py
+++ collatz/test_collatz.py
@@ -2,26 +2,34 @@
 # coding=utf-8
 
 import unittest
+
 from collatz import Collatz
+
 
 class TestCollatz(unittest.TestCase):
     def test_inic_classe(self):
-        self.assertTrue(Collatz())
+        self.assertTrue(isinstance(Collatz(5), int))
 
     def test_dois_deve_retornar_dois_um(self):
-        self.assertEqual(Collatz().retorna_sequencia(2), (2,1))
+        self.assertEqual(Collatz(2).retorna_sequencia(), (2,1))
 
     def test_quatro_retorna_quatro_dois_um(self):
-        self.assertEqual(Collatz().retorna_sequencia(4), (4,2,1))
+        self.assertEqual(Collatz(4).retorna_sequencia(), (4,2,1))
 
     def test_oito_retorna_oito_quatro_dois_um(self):
-        self.assertEqual(Collatz().retorna_sequencia(8), (8,4,2,1))
+        self.assertEqual(Collatz(8).retorna_sequencia(), (8,4,2,1))
 
     def test_dezesseis_retorna_dezesseis_oito_quatro_dois_um(self):
-        self.assertEqual(Collatz().retorna_sequencia(16), (16,8,4,2,1))
+        self.assertEqual(Collatz(16).retorna_sequencia(), (16,8,4,2,1))
 
     def test_impar(self):
-        self.assertEqual(Collatz().retorna_sequencia(5), (5,16,8,4,2,1))
+        self.assertEqual(Collatz(5).retorna_sequencia(), (5,16,8,4,2,1))
+
+    def test_maior_sequencia(self):
+        self.assertEqual(Collatz(5).maior_sequencia(), (3,10,5,16,8,4,2,1))
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
