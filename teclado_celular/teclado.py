20c39a37df4da242b94b93859eacb1972a71e15e
Ramiro Batista da Luz <ramiroluz@gmail.com>
2015-04-25 14:31:50 -0300

    Marcelo e Ramiro string b

--- teclado_celular/teclado.py
+++ teclado_celular/teclado.py
@@ -4,13 +4,21 @@
 import unittest
 
 class TestaTeclado(unittest.TestCase):
-    def testa_teclas(self):
+    def testa_tecla_a(self):
         self.assertEqual(teclas('a'), '2')
+        self.assertEqual(teclas('A'), '2')
+
+    def testa_tecla_b(self):
+        self.assertEqual(teclas('b'), '22')
+        self.assertEqual(teclas('B'), '22')
 
 
 def teclas(frase):
-    if frase == 'a':
+    if frase.upper() == 'A':
         return "2"
+
+    if frase.upper() == 'B':
+        return "22"
 
 
 if __name__ == '__main__':
