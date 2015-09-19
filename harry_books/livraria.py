7589203a8bc3396f46238daf5843f485b710cb4f
Ramiro Batista da Luz <ramiroluz@gmail.com>
2015-09-19 16:27:42 -0300

    Primeiros testes.

--- harry_books/livraria.py
+++ harry_books/livraria.py
@@ -3,4 +3,17 @@
 #
 
 class Compras(object):
-    pass
+    def __init__(self, quantidade):
+        self.quantidade = quantidade
+        self.preco = 42
+        self._total = 0
+
+    @property
+    def total(self):
+        self._total = self.quantidade * self.preco
+
+        if self.quantidade == 2:
+            self._total *= 0.95
+        else:
+            self._total *= 0.9
+        return self._total
--- harry_books/livraria_test.py
+++ harry_books/livraria_test.py
@@ -8,9 +8,23 @@ from livraria import Compras
 
 
 class TestCompras(unittest.TestCase):
-    def test_modelo(self):
-        compras = Compras()
+    def test_criacao_da_classe(self):
+        compras = Compras(1)
         self.assertFalse(compras is None)
+        self.assertEqual(compras.quantidade, 1)
+        self.assertEqual(compras.preco, 42)
+
+    def test_valor_da_compra_1_livro(self):
+        compras = Compras(1)
+        self.assertEqual(compras.total, 42)
+
+    def test_valor_da_compra_2_livros(self):
+        compras = Compras(2)
+        self.assertEqual(compras.total, (84*0.95))
+
+    def test_valor_da_compra_3_livros(self):
+        compras = Compras(3)
+        self.assertEqual(compras.total, ((42*3)*0.90))
 
 if __name__ == '__main__':
     unittest.main()	
