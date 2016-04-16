bd556081902be846cec2718ed7653ef27eb673b3
Dojo Paraná <dojo-parana@googlegroups.com>
2016-04-16 11:11:04 -0300

    Reinaldo e Laura: Passou o teste inicial

--- poker/poker.py
+++ poker/poker.py
@@ -1,10 +1,12 @@
 import unittest
 
 def poker(jogador1, jogador2):
-    pass
+    return 'Jogador 2'
 
 class PokerTest(unittest.TestCase):
-    pass
+    def test_poker (self):
+        self.assertEquals(poker('5H 5C 6S 7S KD', '2C 3S 8S 8D TD'), 'Jogador 2')
+
 
 if __name__ == '__main__':
     unittest.main()
