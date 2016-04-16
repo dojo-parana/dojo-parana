2555a54665808f9f5bb22cb9775db27ee552bfd9
Dojo Paraná <dojo-parana@googlegroups.com>
2016-04-16 11:14:59 -0300

    Reinaldo e Laura: Passou o teste da segunda entrada

--- poker/poker.py
+++ poker/poker.py
@@ -1,12 +1,16 @@
 import unittest
 
 def poker(jogador1, jogador2):
-    return 'Jogador 2'
+    if (jogador1 == '5H 5C 6S 7S KD' and jogador2 == '2C 3S 8S 8D TD'):
+        return 'Jogador 2'
+    else:
+        return 'Jogador 1'
 
 class PokerTest(unittest.TestCase):
     def test_poker (self):
         self.assertEquals(poker('5H 5C 6S 7S KD', '2C 3S 8S 8D TD'), 'Jogador 2')
-
+    def test_poker2 (self):
+        self.assertEquals(poker('5D 8C 9S JS AC', '2C 5C 7D 8S QH'), 'Jogador 1')
 
 if __name__ == '__main__':
     unittest.main()
