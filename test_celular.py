f81ee03cca088064c48a6fec72423efad4423f84
Ramiro Batista da Luz <ramiroluz@gmail.com>
2017-09-29 09:51:57 -0300

    Commit inicial, Ramiro

---
+++ test_celular.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import unittest
+
+class Test(unittest.TestCase):
+    def test_init(self):
+        self.assertTrue(True)
+
+
+if __name__ == '__main__':
+    unittest.main()
